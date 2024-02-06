53544d1a7a8f15f2922c519a232f1047f1c97b6a
HibbanHaroon <hibbanharoon@gmail.com>
2024-02-06 23:06:37 +0500

    Add Quote to the GoalTracker page

--- src/pages/GoalTracker/components/Content.js
+++ src/pages/GoalTracker/components/Content.js
@@ -5,23 +5,25 @@ import { StyledEngineProvider } from "@mui/material/styles";
 import "./styles/Content.css";
 import FlipClock from "./FlipClock";
 import Spotify from "./Spotify";
+import Quote from "./Quote";
 
 function Content() {
   return (
     <div className="main-content">
       {/* Top Container is for the all the interactice content, such as goals, spotify player, calendar etc. */}
       <div className="top-container">
-        <div className="side-container">
+        <div className="left-container">
           <FlipClock />
           <Spotify />
         </div>
         <div className="to-do-list">
           <GoalList />
         </div>
-        <div className="calendar-container">
+        <div className="right-container">
           <StyledEngineProvider injectFirst>
             <Calendar />
           </StyledEngineProvider>
+          <Quote />
         </div>
       </div>
     </div>
