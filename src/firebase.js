0e77c5830a6cd0215eacfa8d860af8646183917b
HibbanHaroon <hibbanharoon@gmail.com>
2024-02-11 06:30:01 +0500

    Add Favicon and Remove Unnecessary Files

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIh1DFeckIGyMjbcHn-BHE8TRpjag2Psc",
-  authDomain: "goal-tracker-0001.firebaseapp.com",
-  projectId: "goal-tracker-0001",
-  storageBucket: "goal-tracker-0001.appspot.com",
-  messagingSenderId: "514651992485",
-  appId: "1:514651992485:web:a54713f21c07cd1c59384c",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
