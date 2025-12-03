8298c9f1b8eb3c03847cbf3b9870f3e79e915c95
HibbanHaroon <hibbanharoon@gmail.com>
2025-12-02 18:57:01 -0600

    Fix for redirect added to firebase.js as well

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { IS_DEV } from "./constants";
+
+/**
+ * For signInWithRedirect to work on Vercel:
+ * - In development: use Firebase's default authDomain
+ * - In production: use the current domain (Vercel) so redirects go through our proxy
+ */
+const getAuthDomain = () => {
+  if (IS_DEV) {
+    return process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+  }
+  // In production, use the current hostname for redirect to work through Vercel proxy
+  return window.location.hostname;
+};
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: getAuthDomain(),
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
